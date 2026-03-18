d6be5058058a45a68a9fd13bc2efa08898cd5042
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-18 12:27:16 +0100

    Add models to admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Requester, Pet, Booking, Review
 
 # Register your models here.
+admin.site.register(Requester)
+admin.site.register(Pet)
+admin.site.register(Booking)
+admin.site.register(Review)
