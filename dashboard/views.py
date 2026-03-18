4a3e6407fc6330b6c333bccc280fdfee6a2547a9
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-18 16:22:35 +0100

    Created classes for views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Requester, Pet, Booking, Review
+from .serializers import RequesterSerializer, PetSerializer, BookingSerializer, ReviewSerializer
 
-# Create your views here.
+class RequesterViewSet(viewsets.ModelViewSet):
+    queryset = Requester.objects.all()
+    serializer_class = RequesterSerializer
+    
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
