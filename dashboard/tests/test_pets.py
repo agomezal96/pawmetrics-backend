a4875aa89da7b8faf43da9ce79a0122be9ca1415
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-23 17:03:03 +0100

    Fixed import issues

--- dashboard/tests/test_pets.py
+++ dashboard/tests/test_pets.py
@@ -1,8 +1,7 @@
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Requester, Pet
+from ..models import Requester, Pet
 
 # We use rest_framework.test.APITestCase.
 # Think of this as a "Super-Tool" that:
