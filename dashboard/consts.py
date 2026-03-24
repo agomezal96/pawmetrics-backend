b011c1f66b2fc7dc5e79c7bd930a379920c4a160
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-24 14:39:19 +0100

    create BookingService textchoice

--- dashboard/consts.py
+++ dashboard/consts.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 class PetSpecies(models.TextChoices):
-    DOG = "dog"
-    CAT = "cat"
+    # Format: "db_value", "pretty_label"
+    DOG = "dog", "Dog"
+    CAT = "cat", "Cat"
+
+class BookingService(models.TextChoices):
+    BOARDING = "boarding", "Boarding"
+    HOUSE_SITTING = "sitting", "House Sitting"
+    DROP_IN = "drop_in", "Drop-in Visits"
+    DAYCARE = "daycare", "Doggy Day Care"
+    WALKING = "walking", "Dog Walking"
