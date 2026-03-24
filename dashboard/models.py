f3c422d20233022767c84aa0e46f72c720cf417f
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-24 15:12:28 +0100

    Remove default attribute from price in Booking model

--- dashboard/models.py
+++ dashboard/models.py
@@ -39,7 +39,7 @@ class Booking(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"Booking for {self.pet.name}. Starts: {self.start_date.date()}. Finishes: {self.end_date.date()}"
